5bd30469402b8098d420d28f71385b27388bff77
Samy Nassar <samy0ayman@gmail.com>
2021-07-22 00:23:42 +0200

    Implement Front-end.
    	- Adding React to project.
    	- Adding bootstrap.
    	- Create TrendingLanguages component which create a list of
    	  the trending languages.

---
+++ src/client/scripts/index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount(){
+    fetch('/trending-languages')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+          this.setState({data})
+        })
+  }
+
+  // Create unorder list for Repos url.
+  ReposUrls = (prop) => {
+    const urls = prop.urls;
+    const repoUrlList = urls.map((url) => 
+      <li key={Math.random() * 1000}>
+        <a href={url} target="_blank">{url}</a>
+      </li>
+    )
+    console.log(repoUrlList)
+    return(
+      <ul>
+        {repoUrlList}
+      </ul>
+    )
+  }
+
+  // Create Accordion items. 
+  TerndingList = (prop) => {
+    return(
+      <ol className="list-group">
+        <li className="list-group-item d-flex justify-content-between align-items-start">
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">{prop.language}</div>
+            <this.ReposUrls urls={prop.reposLink} />
+          </div>
+          <span className="badge bg-primary rounded-pill">{prop.reposLink.length}</span>
+        </li>
+      </ol>
+    );
+  }
+
+  TrendingLanguages = (prop) => {
+    const languages = prop.languages;
+    
+    const trending = Object.keys(languages).map((key) => {
+      return(
+        <this.TerndingList key={key} language={key} reposLink={languages[key]} />
+        )
+      })
+      
+    return(trending)
+  }
+
+  render() {
+
+    if(!this.state.data){
+      return (
+        <div class="d-flex justify-content-center text-primary m-2">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      )
+    }
+
+    return (
+        <div>
+          <this.TrendingLanguages languages={this.state.data} />
+        </div>
+      );
+    }
+}
+
+
+
+ReactDOM.render(<App/>, document.getElementById('main'));
